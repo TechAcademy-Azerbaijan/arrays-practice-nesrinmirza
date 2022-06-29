f0697ad236a0985482fa031208c7c32bc9a9f456
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-06-29 01:38:44 -0700

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -10,8 +10,7 @@ prompt.get('input', function (err, result) {
     */
     var input = result.input; // "5,2 4 7 12 9"
     var input_data = input.split(','); // ['5', '2 4 7 12 9']
-    var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
-    var arr = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+   
     let arr2 = []
     for (let i = 0; i < arr.length; i++) {
        if (!arr2.includes(arr[i])) {
